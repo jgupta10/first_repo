e6d84aa620ad80ae78f68bdd8d1d41844edcf591
Jahnavi <jgupta10@jh.edu>
2026-09-01 14:35:42 +0000

    Adding in my homework

---
+++ day1hw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+myfilename=$1
+
+if [ -z "$myfilename" ]; then
+  echo "Please provide a filename as the first command line argument."
+  exit 1
+fi
+
+echo "You provided a filename: $myfilename"
+
+if [ -f "$myfilename" ]; then
+  echo "The file $myfilename exists."
+  humanreadable=$(du -h "$myfilename" | cut -f1)
+  echo "File size: $humanreadable"
+
+  numlines=$(wc -l < "$myfilename")
+  echo "Number of lines: $numlines"
+
+  numchars=$(wc -m < "$myfilename")
+  echo "Number of characters: $numchars"
+
+  numATATATAT=$(grep -c "ATATATAT" "$myfilename")
+  echo "ATATATAT count: $numATATATAT"
+else
+  echo "Error: The file $myfilename does not exist."
+  exit 1
+fi
